47850a18275c93acedbd13d552563df817f2f8a4
Moritz A. Vieli <moritz.vieli@gmail.com>
2026-04-10 15:24:39 +0200

    Update DefaultDiskSpaceService.java

--- src/main/java/com/ascargon/rocketshow/util/DefaultDiskSpaceService.java
+++ src/main/java/com/ascargon/rocketshow/util/DefaultDiskSpaceService.java
@@ -13,35 +13,53 @@ import java.io.InputStreamReader;
 @Service
 public class DefaultDiskSpaceService implements DiskSpaceService {
 
-	@Override
-	public DiskSpace get() throws Exception {
-		DiskSpace diskSpace = new DiskSpace();
+    private final DeviceInformationService deviceInformationService;
 
-		// Get the used and available space in MB
-		ProcessBuilder pb = new ProcessBuilder("df", "-BMB", "/");
-		Process process = pb.start();
-		BufferedReader br = new BufferedReader(new InputStreamReader(process.getInputStream()));
-		String line;
+    public DefaultDiskSpaceService(
+            DeviceInformationService deviceInformationService
+    ) {
+        this.deviceInformationService = deviceInformationService;
+    }
 
-		// Loop over each line
-		while ((line = br.readLine()) != null) {
-			line = line.replaceAll("\t", " ");
-			line = line.replaceAll(" +", " ");
-			line = line.replaceAll("MB", "");
+    @Override
+    public DiskSpace get() throws Exception {
+        DiskSpace diskSpace = new DiskSpace();
+        String root = "/";
 
-			String[] parts = line.split(" ");
+        if (deviceInformationService.getDeviceInformation().isAvailable()) {
+            root = "/data";
+        }
 
-			String usedMB = parts[2];
-			String availableMB = parts[3];
-			String drive = parts[5];
+        // Get the used and available space in MB
+        ProcessBuilder pb = new ProcessBuilder("df", "-BMB", root);
+        Process process = pb.start();
+        BufferedReader br = new BufferedReader(new InputStreamReader(process.getInputStream()));
+        String line;
 
-			if("/".equals(drive)) {
-				diskSpace.setUsedMB(Double.parseDouble(usedMB));
-				diskSpace.setAvailableMB(Double.parseDouble(availableMB));
-			}
-		}
+        // Loop over each line
+        while ((line = br.readLine()) != null) {
+            line = line.replaceAll("\t", " ");
+            line = line.replaceAll(" +", " ");
+            line = line.replaceAll("MB", "");
 
-		return diskSpace;
-	}
+            String[] parts = line.split(" ");
+
+            // skip header line
+            if (parts.length < 6 || "Used".equalsIgnoreCase(parts[2])) {
+                continue;
+            }
+
+            String usedMB = parts[2];
+            String availableMB = parts[3];
+            String drive = parts[5];
+
+            if (root.equals(drive)) {
+                diskSpace.setUsedMB(Double.parseDouble(usedMB));
+                diskSpace.setAvailableMB(Double.parseDouble(availableMB));
+            }
+        }
+
+        return diskSpace;
+    }
 
 }
